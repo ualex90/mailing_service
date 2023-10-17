0c3aa15e9f26c224c677596255317d555272319a
u_alex90 <u_alex90@mail.ru>
2023-10-17 23:56:20 +1000

    create models

--- customers_app/models.py
+++ customers_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    last_name = models.CharField(max_length=150, verbose_name='фамилия')
+    first_name = models.CharField(max_length=150, verbose_name='имя')
+    surname = models.CharField(max_length=150, blank=True, null=True, verbose_name='отчество')
+    email = models.EmailField(max_length=100, verbose_name='email')
+    comment = models.TextField(blank=True, null=True, verbose_name='комментарий')
+    is_mailing = models.BooleanField(default=True, verbose_name='подписка на рассылку')
+
+    def __str__(self):
+        return f'{self.last_name} {self.first_name} {self.surname}'
+
+    class Meta:
+        verbose_name = 'клиента'
+        verbose_name_plural = 'клиенты'
--- service_app/models.py
+++ service_app/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+from customers_app.models import Customer
+
+
+class Mailing(models.Model):
+    name = models.CharField(max_length=150, verbose_name='название рассылки')
+    title = models.CharField(max_length=150, verbose_name='тема письма')
+    body = models.TextField(verbose_name='тело письма')
+    periodicity = models.CharField(max_length=5, verbose_name='периодичность')  # day, week, month
+    mailing_time = models.TimeField(verbose_name='время рассылки')
+    customers = models.ManyToManyField(Customer, verbose_name='клиенты')
+    status = models.CharField(max_length=10, blank=True, null=True, verbose_name='статус')  # completed, created, launched
+
+    def __str__(self):
+        return f'{self.mailing_time} {self.periodicity}'
+
+    class Meta:
+        verbose_name = 'рассылку'
+        verbose_name_plural = 'рассылки'
+
+
+class Log(models.Model):
+    mailing = models.ForeignKey(Mailing, on_delete=models.CASCADE, verbose_name='рассылка')
+    last_attempt = models.DateTimeField(blank=True, null=True, verbose_name='дата и время последней попытки')
+    attempt_status = models.CharField(max_length=20, blank=True, null=True, verbose_name='статус попытки')
+    server_response = models.IntegerField(blank=True, null=True, verbose_name='ответ почтового сервера, если он был')
+
+    def __str__(self):
+        return self.last_attempt
+
+    class Meta:
+        verbose_name = 'попытку'
+        verbose_name_plural = 'попытки'
