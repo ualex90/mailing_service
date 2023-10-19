1ea33142e82444edb40118fbbbee0f0173cd3045
u_alex90 <u_alex90@mail.ru>
2023-10-19 21:52:38 +1000

    create controllers

--- customers_app/views.py
+++ customers_app/views.py
@@ -1,3 +1,68 @@
 from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DetailView, DeleteView
 
-# Create your views here.
+from customers_app.models import Customer
+
+
+class CustomerListView(ListView):
+    model = Customer
+    queryset = Customer.objects.filter().order_by('pk').reverse()
+    extra_context = {
+        'title': 'Клиенты',
+        'description': 'Список клиентов',
+    }
+
+
+class CustomerDetailView(DetailView):
+    model = Customer
+    extra_context = {
+        'description': 'Карточка клиента',
+    }
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        post_item = Customer.objects.get(pk=self.kwargs.get('pk'))
+        context_data['title'] = f'{post_item.last_name}, {post_item.first_name}, {post_item.surname}'
+        return context_data
+
+
+class CustomerCreateView(CreateView):
+    model = Customer
+    fields = ['last_name', 'first_name', 'surname', 'email', 'comment', 'subscriptions',]
+    success_url = reverse_lazy('customers_app:list')
+    extra_context = {
+        'title': 'Клиент',
+        'description': 'Добавить клиента',
+    }
+
+
+class CustomerUpdateView(UpdateView):
+    model = Customer
+    fields = ['last_name', 'first_name', 'surname', 'email', 'comment', 'subscriptions',]
+    extra_context = {
+        'description': 'Изменить клиента',
+    }
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        post_item = Customer.objects.get(pk=self.kwargs.get('pk'))
+        context_data['title'] = f'{post_item.last_name}, {post_item.first_name}, {post_item.surname}'
+        return context_data
+
+    def get_success_url(self):
+        return reverse('customers_app:detail', args=[self.object.pk])
+
+
+class CustomerDeleteView(DeleteView):
+    model = Customer
+    success_url = reverse_lazy('customers_app:list')
+    extra_context = {
+        'description': 'Удаление пользователя',
+    }
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        post_item = Customer.objects.get(pk=self.kwargs.get('pk'))
+        context_data['title'] = f'{post_item.last_name}, {post_item.first_name}, {post_item.surname}'
+        return context_data
--- service_app/views.py
+++ service_app/views.py
@@ -1,12 +1,48 @@
-from django.views.generic import ListView
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse, reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView
 
 from service_app.models import Mailing
 
 
 class MailingListView(ListView):
     model = Mailing
+    queryset = Mailing.objects.filter().order_by('pk').reverse()
     template_name = 'service_app/index.html'
+
     extra_context = {
         'title': 'Рассылки',
         'description': 'Список рассылок',
     }
+
+
+class MailingCreateView(CreateView):
+    model = Mailing
+    success_url = reverse_lazy('service_app:index')
+    fields = ['name', 'title', 'body', 'periodicity', 'mailing_time', 'status', 'is_active',]
+    extra_context = {
+        'title': 'Рассылки',
+        'description': 'Создание новой рассылки',
+    }
+
+
+class MailingUpdateView(UpdateView):
+    model = Mailing
+    fields = ['name', 'title', 'body', 'periodicity', 'mailing_time', 'status', 'is_active', ]
+    success_url = reverse_lazy('service_app:index')
+    extra_context = {
+        'title': 'Рассылки',
+        'description': 'Изменение рассылки',
+    }
+
+
+def toggle_activity(request, pk):
+    item = get_object_or_404(Mailing, pk=pk)
+    if item.is_active:
+        item.is_active = False
+    else:
+        item.is_active = True
+
+    item.save()
+
+    return redirect(reverse('service_app:index'))
