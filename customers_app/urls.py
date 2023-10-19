68eb9633ce401c9ef3ec35a941769415d8544ac0
u_alex90 <u_alex90@mail.ru>
2023-10-19 21:52:52 +1000

    create urls

--- customers_app/urls.py
+++ customers_app/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
 from customers_app.apps import CustomersAppConfig
+from customers_app.views import CustomerListView, CustomerDetailView, CustomerCreateView, CustomerUpdateView, \
+    CustomerDeleteView
 
 app_name = CustomersAppConfig.name
 
 urlpatterns = [
-    # path('list', ..., name='list'),
-    # path('customer/<int:pk>', ..., name='customer'),
+    path('list/', CustomerListView.as_view(), name='list'),
+    path('customer/<int:pk>', CustomerDetailView.as_view(), name='customer'),
+    path('create/', CustomerCreateView.as_view(), name='create'),
+    path('update/<int:pk>', CustomerUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', CustomerDeleteView.as_view(), name='delete'),
 ]
--- service_app/urls.py
+++ service_app/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
 from service_app.apps import ServiceAppConfig
-from service_app.views import MailingListView
+from service_app.views import MailingListView, MailingCreateView, MailingUpdateView, toggle_activity
 
 app_name = ServiceAppConfig.name
 
 urlpatterns = [
     path('', MailingListView.as_view(), name='index'),
+    path('create/', MailingCreateView.as_view(), name='create'),
+    path('update/<int:pk>/', MailingUpdateView.as_view(), name='update'),
+    path('activity/<int:pk>/', toggle_activity, name='toggle_activity'),
 ]
