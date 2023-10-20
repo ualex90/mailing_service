cd008e7bfac73296579a15d686e8ec5ce17d697a
u_alex90 <u_alex90@mail.ru>
2023-10-20 20:50:11 +1000

    add customer detail

--- customers_app/urls.py
+++ customers_app/urls.py
@@ -8,7 +8,7 @@ app_name = CustomersAppConfig.name
 
 urlpatterns = [
     path('list/', CustomerListView.as_view(), name='list'),
-    path('customer/<int:pk>', CustomerDetailView.as_view(), name='customer'),
+    path('customer/<int:pk>', CustomerDetailView.as_view(), name='detail'),
     path('create/', CustomerCreateView.as_view(), name='create'),
     path('update/<int:pk>', CustomerUpdateView.as_view(), name='update'),
     path('delete/<int:pk>', CustomerDeleteView.as_view(), name='delete'),
