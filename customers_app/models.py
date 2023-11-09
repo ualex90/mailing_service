55ffb5ea56534ce562cc72c150348d1d6421589e
u_alex90 <u_alex90@mail.ru>
2023-11-10 00:38:54 +1000

    Add main page

--- customers_app/models.py
+++ customers_app/models.py
@@ -8,13 +8,13 @@ class Customer(models.Model):
     last_name = models.CharField(max_length=150, verbose_name='фамилия')
     first_name = models.CharField(max_length=150, verbose_name='имя')
     surname = models.CharField(**NULLABLE, max_length=150, verbose_name='отчество')
-    email = models.EmailField(max_length=100, unique=True, verbose_name='email')
+    email = models.EmailField(max_length=100, unique=False, verbose_name='email')
     comment = models.TextField(**NULLABLE, verbose_name='комментарий')
     is_mailing = models.BooleanField(default=True, verbose_name='подписка на рассылки')
     owner = models.ForeignKey('users_app.User', **NULLABLE, on_delete=models.CASCADE, verbose_name='создал')
 
     def __str__(self):
-        return f'{self.last_name} {self.first_name} {self.surname}'
+        return self.email
 
     class Meta:
         verbose_name = 'клиента'
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,11 @@
+from django.utils.timezone import now
 from django.views.generic import TemplateView
 
 from blog_app.models import Post
+from customers_app.models import Customer
 from main_app.utils import get_random_list
+from service_app.models import Mailing
+from users_app.models import User
 
 
 class IndexView(TemplateView):
@@ -9,4 +13,26 @@ class IndexView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context_data = super().get_context_data(**kwargs)
+
+        # Количество пользователей сервиса
+        context_data['user_count'] = User.objects.count()
+
+        # Количество рассылок всего
+        context_data['mailing_count'] = Mailing.objects.count()
+
+        # Количество активных рассылок
+        context_data['mailing_active_count'] = (Mailing.objects.
+                                                filter(start_time__lte=now()).
+                                                exclude(status=Mailing.PAUSED).
+                                                exclude(status=Mailing.COMPLETED).
+                                                exclude(is_active=False).count())
+
+        # Количество уникальных клиентов для рассылок
+        customers = set(map(str, Customer.objects.all()))
+        print(customers)
+        context_data['customer_count'] = len(customers)
+
+        # 3 случайных записи из блога
         context_data['post_list'] = get_random_list(Post)
+
+        return context_data
