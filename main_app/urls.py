9c0dd5490ff58c9929fa9309814e9b00115c3745
u_alex90 <u_alex90@mail.ru>
2023-11-10 00:41:49 +1000

    Add cached main page

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from main_app.apps import MainAppConfig
 from main_app.views import IndexView
@@ -6,5 +7,5 @@ from main_app.views import IndexView
 app_name = MainAppConfig.name
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
+    path('', cache_page(60)(IndexView.as_view()), name='index'),
 ]
