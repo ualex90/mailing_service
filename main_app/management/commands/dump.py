2c316137ee76f256d9ec54b5b9cfadfeaa3b4898
u_alex90 <u_alex90@mail.ru>
2023-11-11 02:22:44 +1000

    dump data

--- main_app/management/commands/dump.py
+++ main_app/management/commands/dump.py
@@ -6,6 +6,7 @@ from django.core.management import BaseCommand
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
+        os.system("python3 manage.py dumpdata auth.group > fixtures/auth_grope.json")
         os.system("python3 manage.py dumpdata users_app > fixtures/users_app.json")
         os.system("python3 manage.py dumpdata main_app > fixtures/main_app.json")
         os.system("python3 manage.py dumpdata service_app > fixtures/service_app.json")
--- main_app/management/commands/load.py
+++ main_app/management/commands/load.py
@@ -6,6 +6,7 @@ from django.core.management import BaseCommand
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
+        os.system("python3 manage.py loaddata fixtures/auth_grope.json")
         os.system("python3 manage.py loaddata fixtures/users_app.json")
         os.system("python3 manage.py loaddata main_app > fixtures/main_app.json")
         os.system("python3 manage.py loaddata service_app > fixtures/service_app.json")
