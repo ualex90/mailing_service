7885bbcad4ec9fda9affda589f0d0d838e1aaf27
u_alex90 <u_alex90@mail.ru>
2023-11-11 02:35:41 +1000

    dump data

--- main_app/management/commands/dump.py
+++ main_app/management/commands/dump.py
@@ -6,11 +6,11 @@ from django.core.management import BaseCommand
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
-        os.system("python3 manage.py dumpdata auth.group > fixtures/auth_grope.json")
-        os.system("python3 manage.py dumpdata users_app > fixtures/users_app.json")
-        os.system("python3 manage.py dumpdata main_app > fixtures/main_app.json")
-        os.system("python3 manage.py dumpdata service_app > fixtures/service_app.json")
-        os.system("python3 manage.py dumpdata customers_app > fixtures/customers_app.json")
-        os.system("python3 manage.py dumpdata logger_app > fixtures/logger_app.json")
-        os.system("python3 manage.py dumpdata blog_app > fixtures/blog_app.json")
+        os.system("python3 manage.py python3 manage.py dumpdata --exclude auth.permission --exclude contenttypes > fixtures/db.json")
+        # os.system("python3 manage.py dumpdata users_app > fixtures/users_app.json")
+        # os.system("python3 manage.py dumpdata main_app > fixtures/main_app.json")
+        # os.system("python3 manage.py dumpdata service_app > fixtures/service_app.json")
+        # os.system("python3 manage.py dumpdata customers_app > fixtures/customers_app.json")
+        # os.system("python3 manage.py dumpdata logger_app > fixtures/logger_app.json")
+        # os.system("python3 manage.py dumpdata blog_app > fixtures/blog_app.json")
 
