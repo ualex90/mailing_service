72696e324465130b5502defe5bc6f97744e939bd
u_alex90 <u_alex90@mail.ru>
2023-11-11 02:42:04 +1000

    dump data

--- main_app/management/commands/dump.py
+++ main_app/management/commands/dump.py
@@ -6,7 +6,7 @@ from django.core.management import BaseCommand
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
-        os.system("python3 manage.py python3 manage.py dumpdata --exclude auth.permission --exclude contenttypes > fixtures/db.json")
+        os.system("python3 manage.py dumpdata --exclude auth.permission --exclude contenttypes > fixtures/db.json")
         # os.system("python3 manage.py dumpdata users_app > fixtures/users_app.json")
         # os.system("python3 manage.py dumpdata main_app > fixtures/main_app.json")
         # os.system("python3 manage.py dumpdata service_app > fixtures/service_app.json")
