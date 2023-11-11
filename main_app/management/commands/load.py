0abb3bec89f7f26f3093b986510d23fd0b4650ce
u_alex90 <u_alex90@mail.ru>
2023-11-11 11:59:20 +1000

    fixed dump

--- main_app/management/commands/load.py
+++ main_app/management/commands/load.py
@@ -7,11 +7,4 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         os.system("python3 manage.py loaddata fixtures/db.json")
-        os.system("python3 manage.py loaddata fixtures/auth_grope.json")
-        os.system("python3 manage.py loaddata fixtures/users_app.json")
-        os.system("python3 manage.py loaddata main_app > fixtures/main_app.json")
-        os.system("python3 manage.py loaddata service_app > fixtures/service_app.json")
-        os.system("python3 manage.py loaddata customers_app > fixtures/customers_app.json")
-        os.system("python3 manage.py loaddata logger_app > fixtures/logger_app.json")
-        os.system("python3 manage.py loaddata blog_app > fixtures/blog_app.json")
 
